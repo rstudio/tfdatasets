36c2349f8cd33e52f582927d2d23670abecd697a
jjallaire <jj@rstudio.com>
2017-10-05 08:39:28 -0400

    add some more dataset methods

--- R/dataset_methods.R
+++ R/dataset_methods.R
@@ -58,6 +58,66 @@ dataset_batch <- function(dataset, batch_size) {
 }
 
 
+#' Caches the elements in this dataset.
+#'
+#'
+#' @param dataset A dataset
+#' @param filename String with the name of a directory on the filesystem to use
+#'   for caching tensors in this Dataset. If a filename is not provided, the
+#'   dataset will be cached in memory.
+#'
+#' @return A dataset
+#'
+#' @family dataset methods
+#'
+#' @export
+dataset_cache <- function(dataset, filename = NULL) {
+  if (is.null(filename))
+    filename <- ""
+  if (!is.character(filename))
+    stop("filename must be a character vector")
+  dataset$cache(tf$constant(filename, dtype = tf$string))
+}
+
+
+#' Creates a dataset by concatenating given dataset with this dataset.
+#'
+#' @note Input dataset and dataset to be concatenated should have same nested
+#'   structures and output types.
+#'
+#' @param dataset A dataset
+#' @param other Dataset to be concatenated
+#'
+#' @return A dataset
+#'
+#' @family dataset methods
+#'
+#' @export
+dataset_concatenate <- function(dataset, other) {
+  dataset$concatenate(other)
+}
+
+
+#' Creates a dataset with at most count elements from this dataset
+#'
+#' @param dataset A dataset
+#' @param count Integer representing the number of elements of this dataset that
+#'   should be taken to form the new dataset. If `count` is -1, or if `count` is
+#'   greater than the size of this dataset, the new dataset will contain all
+#'   elements of this dataset.
+#'
+#' @return A dataset
+#'
+#' @family dataset methods
+#'
+#' @export
+dataset_take <- function(dataset, count) {
+  dataset$take(count = as_tensor_int64(count))
+}
+
+
+
+
 
 
 
--- tests/testthat/test-dataset-methods.R
+++ tests/testthat/test-dataset-methods.R
@@ -26,4 +26,24 @@ test_succeeds("dataset methods can be chained", {
     dataset_repeat(4)
 })
 
+test_succeeds("dataset_cache to memory returns a dataset", {
+  dataset <- tensors_dataset(tf$constant(1:100)) %>%
+    dataset_cache()
+})
+
+test_succeeds("dataset_cache to disk returns a dataset", {
+  dataset <- tensors_dataset(tf$constant(1:100)) %>%
+    dataset_cache(filename = tempfile())
+})
+
+test_succeeds("dataset_concatenate returns a dataset", {
+  dataset <- tensors_dataset(tf$constant(1:100)) %>%
+    dataset_concatenate(tensors_dataset(tf$constant(1:100)))
+})
+
+test_succeeds("dataset_take returns a dataset", {
+  dataset <- tensors_dataset(tf$constant(1:100)) %>%
+    dataset_take(50)
+})
+
 
