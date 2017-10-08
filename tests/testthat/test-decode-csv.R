a4a817bd2fc5454561a2e107230771d177d11d5d
jjallaire <jj@rstudio.com>
2017-10-08 16:25:46 -0400

    add some decode_csv tests

--- tests/testthat/test-decode-csv.R
+++ tests/testthat/test-decode-csv.R
@@ -17,6 +17,12 @@ test_that("decode_csv parses explicitly named columns", {
   expect_identical(cols, names(dataset$output_shapes))
 })
 
+test_succeeds("decode_csv handles explicit record default/type specifications", {
+  skip_if_no_tensorflow()
+  csv_dataset("data/mtcars.csv", record_defaults = list(0,0L,0,0L,0,0,0,0L,0L,0L,0L))
+  csv_dataset("data/mtcars.csv", record_defaults = list(0,0,0,0,0,0,0,0,0,0,0))
+})
+
 
 
 
