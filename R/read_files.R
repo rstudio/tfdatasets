9c1364490395062a144878821233fc42fa2c1c2a
jjallaire <jj@rstudio.com>
2017-10-20 11:44:01 -0400

    add test for parallel file reading

--- R/read_files.R
+++ R/read_files.R
@@ -9,19 +9,20 @@
 #' @param reader Function that maps a file into a dataset (e.g.
 #'   [text_line_dataset()] or [tfrecord_dataset()]).
 #' @param ... Additional arguments to pass to `reader` function
-#' @param num_shards An integer representing the number of shards operating in
-#'   parallel.
-#' @param shard_index An integer, representing the worker index.
 #' @param parallel_files An integer, number of files to process in parallel
 #' @param parallel_interleave An integer, number of consecutive records to
 #'   produce from each file before cycling to another file.
+#' @param num_shards An integer representing the number of shards operating in
+#'   parallel.
+#' @param shard_index An integer, representing the worker index. Shared indexes
+#'  are 0 based so for e.g. 8 shards valid indexes would be 0-7.
 #'
 #' @return A dataset
 #'
 #' @export
 read_files <- function(files, reader, ...,
-                       num_shards = 1, shard_index = 1,
-                       parallel_files = 1, parallel_interleave = 1) {
+                       parallel_files = 1, parallel_interleave = 1,
+                       num_shards = NULL, shard_index = NULL) {
 
   # files dataset to process
   files_dataset <- NULL
@@ -54,10 +55,20 @@ read_files <- function(files, reader, ...,
   }
 
   # read with appropriate parallel options
-  dataset <- files_dataset %>%
-    dataset_shard(num_shards = num_shards, index = shard_index) %>%
+  dataset <- files_dataset
+
+  # shard
+  if (!is.null(num_shards)) {
+    dataset <- dataset %>%
+      dataset_shard(num_shards = num_shards, index = shard_index)
+  }
+
+  # parallel files with interleave
+  dataset <- dataset %>%
     dataset_interleave(cycle_length = parallel_files, block_length = parallel_interleave,
-                       function(file) { reader(file, ...) })
+                       function(file) {
+                         reader(file, ...)
+                        })
 
   # return
   as_tf_dataset(dataset)
---
+++ tests/testthat/test-read-files.R
@@ -0,0 +1,29 @@
+context("read-files")
+
+source("utils.R")
+
+test_succeeds("files and be read in parallel", {
+
+  mtcars_spec <- csv_record_spec("data/mtcars.csv")
+
+  dataset <- read_files("data/mtcars-*.csv", text_line_dataset, record_spec = mtcars_spec,
+                        parallel_files = 4, parallel_interleave = 1,
+                        num_shards = 2, shard_index = 0) %>%
+    dataset_batch(1000)
+
+  with_session(function(sess) {
+    batch <- next_batch(dataset)
+    data <- sess$run(batch)
+    expect_equal(as.integer(data$cyl[1:8]), rep_len(6L, 8))
+  })
+
+
+})
+
+
+
+
+
+
+
+
