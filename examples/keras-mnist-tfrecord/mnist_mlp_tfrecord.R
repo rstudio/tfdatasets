5c4bc2fbdd0a51b149164e84771cb948ec419222
jjallaire <jj@rstudio.com>
2017-11-18 21:03:20 -0500

    more image preprocessing

--- examples/keras-mnist-tfrecord/mnist_mlp_tfrecord.R
+++ examples/keras-mnist-tfrecord/mnist_mlp_tfrecord.R
@@ -10,27 +10,25 @@ mnist_dataset <- function(filename) {
         features = list(
           image_raw = tf$FixedLenFeature(shape(), tf$string),
           label = tf$FixedLenFeature(shape(), tf$int64)
-        ))
-      list(
-        image = tf$reshape(
-          tf$decode_raw(features$image_raw, tf$uint8),
-          shape(28, 28)
-        ),
-        label = tf$cast(features$label, tf$int32)
+        )
       )
-    })
-  # %>%
-  #   dataset_batch(128) %>%
-  #   dataset_repeat()
+
+      # preprocess image
+      image <- tf$decode_raw(features$image_raw, tf$uint8)
+      image <- tf$cast(image, tf$float32) / 255
+
+      # convert label to integer
+      label <- tf$cast(features$label, tf$int32)
+
+      # return
+      list(
+        image = image,
+        label = label
+      )
+    }) %>%
+    dataset_batch(128) %>%
+    dataset_repeat()
 }
 
 train_dataset <- mnist_dataset("mnist/train.tfrecords")
 
-
-dataset_map(function(example_proto) {
-  features <- list(
-    image = tf$FixedLenFeature(shape(), tf$string, default_value = ""),
-    label = tf$FixedLenFeature(shape(), tf$int32, default_value = 0L)
-  )
-  tf$parse_single_example(example_proto, features)
-})
