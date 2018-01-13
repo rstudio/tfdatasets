2a1a25e068cbdb8ad1fe0f70e7b3550d84fced3f
jjallaire <jj@rstudio.com>
2018-01-13 07:19:19 -0500

    don't create tf session in tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,7 @@ library(testthat)
 library(tensorflow)
 library(tfdatasets)
 
-# run tests in default tf session
-sess <- tf$Session()
-with(sess$as_default(), {
-  test_check("tfdatasets")
-})
-
+test_check("tfdatasets")
 
 
 
