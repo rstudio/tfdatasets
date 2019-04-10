4e9e38ec7a9c7e73e2cad2e8643d644da1b46907
Daniel Falbel <dfalbel@gmail.com>
2019-04-10 16:00:20 -0300

    add shuffle to avoid warnings in tests

--- tests/testthat/test-dataset-fitting.R
+++ tests/testthat/test-dataset-fitting.R
@@ -2,12 +2,13 @@ context("dataset-fitting")
 
 source("utils.R")
 
-require(keras)
+library(keras)
 
 test_succeeds("can fit a dataset using keras fit", {
   d  <-
     tensor_slices_dataset(list(matrix(1:20, ncol = 2), matrix(1:10, ncol = 1))) %>%
-    dataset_batch(2)
+    dataset_batch(2) %>%
+    dataset_shuffle(1024)
 
   model <- keras_model_sequential() %>%
     layer_dense(units = 1, input_shape = 2)
@@ -19,7 +20,9 @@ test_succeeds("can fit a dataset using keras fit", {
 test_succeeds("can fit a dataset using keras fit_generator", {
   d  <-
     tensor_slices_dataset(list(matrix(1:20, ncol = 2), matrix(1:10, ncol = 1))) %>%
-    dataset_batch(2)
+    dataset_batch(2) %>%
+    dataset_shuffle(1024)
+
 
   model <- keras_model_sequential() %>%
     layer_dense(units = 1, input_shape = 2)
