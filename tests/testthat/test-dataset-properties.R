f3e4e1a9048f79b165f3ad23cff3b3a79282fa44
Daniel Falbel <dfalbel@gmail.com>
2019-04-11 20:01:19 -0300

    Get properties from the next_batch.

--- tests/testthat/test-dataset-properties.R
+++ tests/testthat/test-dataset-properties.R
@@ -4,12 +4,23 @@ context("dataset properties")
 source("utils.R")
 
 test_succeeds("output_types returns types", {
+
   dataset <- tensors_dataset(tf$constant(1:100))
-  output_types(dataset)
+  expect_equal(output_types(dataset), tf$int32)
+
+  dataset <- csv_dataset("data/mtcars.csv")
+  types <- output_types(dataset)
+  expect_equal(types$cyl, tf$int32)
+  expect_equal(types$mpg, tf$float32)
 })
 
 test_succeeds("output_shapes returns shapes", {
   dataset <- tensors_dataset(tf$constant(1:100))
-  output_shapes(dataset)
+  expect_equal(output_shapes(dataset)$as_list(), 100)
+
+  dataset <- csv_dataset("data/mtcars.csv")
+  shapes <- output_shapes(dataset)
+  expect_equal(shapes$cyl$as_list(), list())
+  expect_equal(shapes$mpg$as_list(), list())
 })
 
